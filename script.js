b392e354569bee70fd6a8cd737954214fb02d8ac
Ishmael Sealey <ishmaelsealey@gmail.com>
2023-08-07 10:24:32 -0400

    allows popup to hide if anywhere on screen clicked

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const share = document.querySelector("#share");
+const popupElement = document.querySelector("#popup")
 
 function popup() {
     
-    if (document.querySelector("#popup").style.display === 'flex') {
-        document.querySelector("#popup").style.display = 'none';
+    if (popupElement.style.display === 'flex') {
+        popupElement.style.display = 'none';
         document.querySelector("path").style.fill = 'hsl(217, 19%, 35%)';
         share.style.backgroundColor = 'hsl(210, 46%, 95%)';
     } else {
-        document.querySelector("#popup").style.display = 'flex';
+        popupElement.style.display = 'flex';
         document.querySelector("path").style.fill = 'hsl(210, 46%, 95%)';
         share.style.backgroundColor = 'hsl(214, 17%, 51%)';
     }
 }
 
-share.onclick = popup;
+document.addEventListener('click', (event) => {
+    if (event.target === share) {
+        popup()
+    } else {
+        document.querySelector('#popup').style.display = 'none'
+        document.querySelector("path").style.fill = 'hsl(217, 19%, 35%)';
+        share.style.backgroundColor = 'hsl(210, 46%, 95%)';
+    }
+})
